f6c0fccadedcab7ce78e77daff054ad722e782e3
unknown <muktinathrajbanshi786@gmail.com>
2025-10-22 18:18:50 +0330

    Just started now

---
+++ src/components/Posts.jsx
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+import { getPost } from "../api/PostApi";
+
+export const Posts = () => {
+  const getPostData = async () => {
+    const res = await getPost();
+    console.log(res.data);
+    
+  }
+
+  useEffect(() => {
+    getPostData();
+  }, []);
+  
+}
