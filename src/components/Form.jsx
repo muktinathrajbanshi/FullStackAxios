8b762a874ebffc00ff8b38a503ab47798fb02767
unknown <muktinathrajbanshi786@gmail.com>
2025-10-26 18:40:56 +0330

    added input field successfully

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,3 +1,30 @@
 export const Form = () => {
-    return <h1>Hello Form</h1>;
+    return (
+        <form>
+        <div>
+            <label htmlFor="title"></label>
+            <input 
+            type="text" 
+            name="title" 
+            id="title" 
+            autoComplete="off"
+            placeholder="Add Title"
+            />
+        </div>
+
+        <div>
+            <label htmlFor="body"></label>
+            <input 
+            type="text" 
+            name="" 
+            id="" 
+            placeholder="Add Post"  
+            autoComplete="off"
+            id="body" 
+            name="body"
+            />
+        </div>
+        <button type="submit">Add</button>
+        </form>
+    );
 };
