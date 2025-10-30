06c7163296786b53c2a455892a044dc8a29dce71
unknown <muktinathrajbanshi786@gmail.com>
2025-10-30 06:01:11 +0545

    now it's perfect

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -8,6 +8,15 @@ export const Todo = () => {
             <div>
                 <h1>Welcome to Todo Application</h1>
             </div>
+            <div className="maintain">
+            <div className="todo-box">
+                <input 
+                type="text"
+                placeholder="Enter your data"
+                 />
+            </div>
+            <button className="btn">Add task</button>
+            </div>
         </div>
     </>
   );
